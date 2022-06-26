527f1b96f24b1765361e7afb5069e04c575dd2d7
Muhammad-Hamza <77409746+MuhammadHamza-101@users.noreply.github.com>
2022-06-26 22:32:09 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
   const [loading, setLoading] = useState(false);
   const [values, setValues] = useState([]);
   const [error, setError] = useState(null);
+  const [query, setQuery] = useState("");
+  const [filter, setFilter] = useState("");
+  const [paginate, setpaginate] = useState(8);
 
   useEffect(() => {
     fetch(
@@ -25,6 +28,25 @@ function App() {
   }, []);
 
   const data = Object.values(values);
+  const search_parameters = Object.keys(Object.assign({}, ...data));
+  const filter_items = [...new Set(data.map((item) => item.region))];
+
+  function search(items) {
+    return items.filter(
+      (item) =>
+        item.region.includes(filter) &&
+        search_parameters.some((parameter) =>
+          item[parameter]
+            .toString()
+            .toLowerCase()
+            .includes(query)
+        )
+    );
+  }
+  const load_more = (event) => {
+    setpaginate((prevValue) => prevValue + 8);
+  };
+
   if (error) {
     return <>{error.message}</>;
   } else if (!loading) {
@@ -32,31 +54,62 @@ function App() {
   } else {
     return (
       <div className="wrapper">
+        <div className="search-wrapper">
+          <label htmlFor="search-form">
+            <input
+              type="search"
+              name="search-form"
+              id="search-form"
+              className="search-input"
+              placeholder="Search for..."
+              onChange={(e) => setQuery(e.target.value)}
+            />
+            <span className="sr-only">Search countries here</span>
+          </label>
+
+          <div className="select">
+            <select
+              onChange={(e) => setFilter(e.target.value)}
+              className="custom-select"
+              aria-label="Filter Countries By Region"
+            >
+              <option value="">Filter By Region</option>
+              {filter_items.map((item) => (
+                <option value={item}>Filter By {item}</option>
+              ))}
+            </select>
+            <span className="focus"></span>
+          </div>
+        </div>
+
         <ul className="card-grid">
-          {data.map((item) => (
-            <li key={item.alpha3Code}>
-              <article className="card">
-                <div className="card-image">
-                  <img src={item.flag.large} alt={item.name} />
-                </div>
-                <div className="card-content">
-                  <h2 className="card-name">{item.name}</h2>
-                  <ol className="card-list">
-                    <li>
-                      population: <span>{item.population}</span>
-                    </li>
-                    <li>
-                      Region: <span>{item.region}</span>
-                    </li>
-                    <li>
-                      Capital: <span>{item.capital}</span>
-                    </li>
-                  </ol>
-                </div>
-              </article>
-            </li>
-          ))}
+          {search(data)
+            .slice(0, paginate)
+            .map((item) => (
+              <li key={item.alpha3Code}>
+                <article className="card">
+                  <div className="card-image">
+                    <img src={item.flag.large} alt={item.name} />
+                  </div>
+                  <div className="card-content">
+                    <h2 className="card-name">{item.name}</h2>
+                    <ol className="card-list">
+                      <li>
+                        population: <span>{item.population}</span>
+                      </li>
+                      <li>
+                        Region: <span>{item.region}</span>
+                      </li>
+                      <li>
+                        Capital: <span>{item.capital}</span>
+                      </li>
+                    </ol>
+                  </div>
+                </article>
+              </li>
+            ))}
         </ul>
+        <button onClick={load_more}>Load More</button>
       </div>
     );
   }
